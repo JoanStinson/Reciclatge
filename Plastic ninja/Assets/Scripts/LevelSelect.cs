14ef149c509622841720516943b9a0dff8e21860
JohnnyGinard <jgm7.hd@gmail.com>
2018-12-13 12:52:55 +0100

    Improved Main Menu and Level Select and Added Music and Sfx and buttons

---
+++ Plastic ninja/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+    public void Level1() {
+        Game.level = 1;
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level2() {
+        Game.level = 2;
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3() {
+        Game.level = 3;
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level4() {
+        Game.level = 4;
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level5() {
+        Game.level = 5;
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Plastic ninja/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Medals() {
+
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
