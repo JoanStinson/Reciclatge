b2581ca9815f519b03bc94d776b687698a77f9b3
JoanGinard <jgm7.hd@gmail.com>
2018-12-13 20:11:29 +0100

    Medals panel

--- Plastic ninja/Assets/Scripts/MainMenu.cs
+++ Plastic ninja/Assets/Scripts/MainMenu.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
+    public GameObject medalsPanel;
+
     public void Play() {
         SceneManager.LoadScene(1);
     }
 
     public void Medals() {
-
+        medalsPanel.SetActive(true);
     }
 
     public void Exit() {
         Application.Quit();
+    }
+
+    public void CloseMedalsPanel() {
+        medalsPanel.SetActive(false);
     }
 }
