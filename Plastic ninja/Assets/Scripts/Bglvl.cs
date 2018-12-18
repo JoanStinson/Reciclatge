352e5336d6b0b77cfa63b4c55b855f55f1162333
Huiyuan <Huiyuan_97@hotmail.com>
2018-12-18 17:00:29 +0100

    Background done. Bugg fixed. Ready pa empezar de 0

---
+++ Plastic ninja/Assets/Scripts/Bglvl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bglvl : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Game.level == 1)
+        {
+            cam.backgroundColor = Color.green;
+        }
+        if (Game.level == 2)
+        {
+            cam.backgroundColor = Color.blue;
+        }
+        if (Game.level == 3)
+        {
+            cam.backgroundColor = Color.yellow;
+        }
+        if (Game.level == 4)
+        {
+            cam.backgroundColor = Color.grey;
+        }
+        if (Game.level == 5)
+        {
+            cam.backgroundColor = Color.gray;
+        }
+    }
+}
